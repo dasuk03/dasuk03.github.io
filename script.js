cdeb982095797e38988b6eb2b7da1635f5162e84
Daniil Dorofeev <79141512+dasuk03@users.noreply.github.com>
2025-11-11 13:35:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -592,8 +592,6 @@ function triggerDownload(blob, filename = 'cosmozar-export.csv') {
     }, 150);
 }
 
-
-
 function showPage(id) {
     document.querySelectorAll('.page').forEach(p=>p.classList.remove('active'));
     document.getElementById(id).classList.add('active');
